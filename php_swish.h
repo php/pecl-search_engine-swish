876bf8283004f9c55155cd0b3f8c3f580b3d293e
Antony Dovgal <tony2001@php.net>
2007-02-09 16:29:07 +0000

    update copyrights fix API description

--- php_swish.h
+++ php_swish.h
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2006 The PHP Group                                |
+  | Copyright (c) 1997-2007 The PHP Group                                |
   +----------------------------------------------------------------------+
   | This source file is subject to version 3.01 of the PHP license,      |
   | that is bundled with this package in the file LICENSE, and is        |
@@ -12,7 +12,8 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author:                                                              |
+  | Authors: Wez Furlong <wez@omniti.com>                                |
+  |          Antony Dovgal <tony2001@php.net>                            |
   +----------------------------------------------------------------------+
 */
 
